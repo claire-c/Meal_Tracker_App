dc6b34aca8f237e2e9d40bf8da7d142bbbd872e6
Claire <cconnachan@gmail.com>
2018-04-30 16:44:39 +0100

    have a basic spinner working, nothing is broken. Still need to hook up to Java

--- app/src/main/java/com/cconnachan/bottomnavpractice/AddFoodFragment.java
+++ app/src/main/java/com/cconnachan/bottomnavpractice/AddFoodFragment.java
@@ -7,12 +7,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddFoodFragment extends Fragment {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> spinnerAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_food, null);
+
+
+        View viewToInflate = inflater.inflate(R.layout.fragment_add_food, null);
+        //This is initializing my spinner.
+        spinner = viewToInflate.findViewById(R.id.spinner);
+        //This is getting the stuff I need to put in the spinner and give it a look.
+        spinnerAdapter = ArrayAdapter.createFromResource(getContext(), R.array.meal_types, android.R.layout.simple_spinner_item);
+        //This is to give the spinner dropdown items a look.
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //This is to set the adapter
+        spinner.setAdapter(spinnerAdapter);
+
+        //This is what happens when there is a click on the dropdown. It's an override method.
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), parent.getItemAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return viewToInflate;
     }
 }
